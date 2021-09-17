19c45687e2159c1a0b79a7c0581db0ce52f3f9b9
Deinyon Davies <deinyon@canva.com>
2021-09-18 09:58:03 +1000

    Respond to messages

--- index.ts
+++ index.ts
@@ -1,11 +1,34 @@
 import { Client, Intents } from 'discord.js';
 
-const authConfig: {token: string} = require('./auth.json');
+const {
+    token,
+    prefix
+}: {
+    token: string,
+    prefix: string
+} = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
 	console.log('OwO hewo');
 });
+client.once('reconnecting', () => {
+    console.log('Reconnecting ...');
+});
+client.once('disconnect', () => {
+    console.log('Disconnect');
+});
 
-client.login(authConfig.token);
+client.on("messageCreate", async (message) => {
+    if(message.author.bot) {
+        return;
+    }
+    if(!message.content.startsWith(prefix)) {
+        return;
+    }
+
+    message.channel.send(`OwO hewo ${message.author.username}`);
+});
+
+client.login(token);
